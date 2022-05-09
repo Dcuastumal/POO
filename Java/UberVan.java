117268d9e7a2f9c83febde344d4fa749699bc6ef
David Bucheli <davincho1406@gmail.com>
2022-05-09 02:03:57 -0500

    Se aplico herencia en las carpetas de JS y Java

---
+++ Java/UberVan.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+class UberVan extends Car {
+    Map<String, Map<String, Integer>> typeCarAccepted;
+    ArrayList<String> SeatsMaterial;
+
+    public UberVan(String license, Account driver, Map<String, Map<String, Integer>> typeCarAccepted, ArrayList<String> SeatsMaterial){
+        super(license, driver);
+        this.typeCarAccepted = typeCarAccepted;
+        this.SeatsMaterial = SeatsMaterial;
+    }
+}
--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,11 @@
 var car = new Car("AW456", new Account("Andres Herrera", "QWE234"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("NFD321", new Account("Jesus Cortaza", "JECO563"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
+
+var uberX2 = new UberX("JKA785", new Account("Luis Diaz", "LUDI340"), "Toyota", "TXL")
+uberX2.passenger = 4;
+uberX2.printDataCar();
